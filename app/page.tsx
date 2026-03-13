192bb3cc9cee6f8b5520e00078198ad3fa483bb5
Rivaran <mailto:12345678+username@users.noreply.github.com>
2026-03-13 17:47:06 +0900

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1467,7 +1467,7 @@ function startTimer() {
             gap: 10,*/
             display: "grid",
             gridTemplateColumns: "repeat(3, 1fr)",
-            gap: 8,
+            gap: 4,
             justifyContent: "center"
           }}
         >
